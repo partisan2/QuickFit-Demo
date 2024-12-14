5426a9820dd9224726b92f2dc33b107d03db807d
Vihan Gayathra <s92068813@ousl.lk>
2024-12-14 17:26:13 +0530

    build file added

--- MiniProject/obj/Debug/net8.0-windows/MiniProject.AssemblyInfo.cs
+++ MiniProject/obj/Debug/net8.0-windows/MiniProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ff3c3f68b972dbdcedab1d69d9f89dd89e283f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
